ea0a16c97cc7902a0567a9994872fdda9f725c40
Jay <JayKleine@gmail.com>
2023-07-26 20:07:06 -0400

    Employee Class

--- AmbuPro/src/main.java
+++ AmbuPro/src/main.java
@@ -3,7 +3,9 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		HospitalEmployee vito = new HospitalEmployee("Vito", 123);
 
+		System.out.println(vito);
 	}
 
 }
