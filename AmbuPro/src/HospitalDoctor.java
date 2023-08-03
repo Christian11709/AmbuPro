0fbebfe0f9d5eb00c65470434ea3a7122ef33aa2
Jay <JayKleine@gmail.com>
2023-08-03 11:22:15 -0400

    Doctor Class added

---
+++ AmbuPro/src/HospitalDoctor.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+
+public class HospitalDoctor {
+    protected String name;
+    protected int number;
+    protected String assignment;
+    protected static ArrayList<HospitalDoctor> doctors = new ArrayList<>();
+
+    public HospitalDoctor(String docName, int docNumber)
+   {
+      name = docName;
+      number = docNumber;
+      doctors.add(this);
+   }
+
+   public void setAssignment(String docAssignment)
+   {
+      assignment = docAssignment;
+   }
+
+   public void setName(String docName)
+   {
+      name = docName;
+   }
+
+   public void setNumber(int docNumber)
+   {
+      number = docNumber;
+   }
+
+   public String getAssignment(int docAssignment)
+   {
+     return assignment;
+   }
+
+   public String getName()
+   {
+      return name;
+   }
+
+   public int getNumber()
+   {
+      return number;
+   }
+
+   public String toString()
+   {
+      return name + "\t" + number;
+   }
+
+   public void work()
+   {
+      System.out.println(name + " is a doctor that works for the hospital.");
+   }
+
+   public static void listStatus() {
+      for (HospitalDoctor doctor : doctors) {
+          System.out.println("Name: Dr. " + doctor.name + "\tNumber: " + doctor.number + "\tAssignment: " + doctor.assignment);
+      }
+  }
+}
+
--- AmbuPro/src/main.java
+++ AmbuPro/src/main.java
@@ -8,25 +8,34 @@ public class main {
 		HospitalEmployee vito = new HospitalEmployee("Vito", 1231);
 		HospitalEmployee mario = new HospitalEmployee("Mario", 1232);
 		HospitalEmployee luigi = new HospitalEmployee("Luigi", 1233);
-		Ambulance amOne = new Ambulance(true, 1);
-		Ambulance amTwo = new Ambulance(true, 2);
-		Ambulance amThree = new Ambulance(true, 3);
+		HospitalDoctor bowser = new HospitalDoctor("Bowser", 2231);
+		HospitalDoctor drybones = new HospitalDoctor("DryBones", 2232);
+		HospitalDoctor pepper = new HospitalDoctor("Pepper", 2233);
+		Ambulance amOne = new Ambulance(false, 1);
+		Ambulance amTwo = new Ambulance(false, 2);
+		Ambulance amThree = new Ambulance(false, 3);
 
 		//list of every employee + info
 		HospitalEmployee.listStatus();
+		//list of every doctor + info
+		HospitalDoctor.listStatus();
 		//list of every ambulance + info
 		Ambulance.listStatus();
 
-		//setting an employee assignment
+		//set an employee assignment
 		vito.setAssignment("Ambulance #2");
 		System.out.println("Vito is assigned to " + vito.getAssignment(0));
+		
+		//set a doctor assignment
+		pepper.setAssignment("Ambulance #2");
 
-		//make an ambulance unavailable
-		amOne.setStatus(false);
-		System.out.println("Ambulance #1 set to unavailable");
+		//Ambulance #2 is now staffed. set availability to true
+		amOne.setStatus(true);
+		System.out.println("Ambulance #1 set to available");
 
 		//confirm changes
 		HospitalEmployee.listStatus();
+		HospitalDoctor.listStatus();
 		Ambulance.listStatus();
 
 
